a6cebfcf838cda5862a26855dc07d8d87a8486ea
Padebeselis <gintare.kuksenaite@gmail.com>
2024-05-06 20:15:08 +0300

    Started model building

--- help_tool/help_tool.py
+++ help_tool/help_tool.py
@@ -18,6 +18,7 @@ from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
 pd.plotting.register_matplotlib_converters()
 import os
+from scipy.stats import chi2_contingency
 
 
 """Statistics"""
@@ -53,7 +54,47 @@ def first_look(df: pd.DataFrame) -> None:
     return info_df.T
 
 
+def cabin_split(df):
+    # Form deck/num/side, where side can be either P for Port or S for Starboard.
+    df[['Deck', 'Deck_num', 'Side']] = df['Cabin'].str.split('/', expand=True)
+
+    # Available Deck values are A-G, T
+    df['Deck'] = df['Deck'].replace({'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'T': -1})
+
+    # Side has only 2 values and will be updated to bool feature
+    df.loc[df['Side'] == 'S', 'Side_S'] = 1
+    df.loc[df['Side'] == 'P', 'Side_S'] = 0   
+
+    for feature in ['Deck_num']:
+        try:
+            df[feature] = df[feature].astype(int)
+        except: 
+            df[feature] = df[feature].astype(float)
+
+    df.drop(columns=['Side'], inplace=True) 
+    return df
+
+
+def cleaning(df, binary_features):
+    # Each Id takes the form gggg_pp where gggg indicates a group the passenger is travelling with and pp is their number within the group.
+    df[['Group', 'Group_Id']] = df['PassengerId'].str.split('_', expand=True)
+
+    # Separating last name for family identifications
+    df[['First_Name', 'Last_Name']] = df['Name'].str.split(' ', expand=True)
+
+    cabin_split(df)
+
     
+    for feature in binary_features + ['Group', 'Group_Id']:
+        try:
+            df[feature] = df[feature].astype(int)
+        except: 
+            df[feature] = df[feature].astype(float)
+
+
+    df.drop(columns=['PassengerId', 'Name'], inplace=True)
+    return df
+   
 
 
 def distribution_check(df: pd.DataFrame) -> None:
@@ -133,4 +174,72 @@ def phi_corr_matrix(df, feature_list):
     sns.heatmap(filtered_matrix.astype(float), annot=True, annot_kws={"size": 8},
                 cmap='rocket', fmt=".2f", vmin=-1, vmax=1)  # Adjust vmin and vmax as needed
     plt.title(f'Phi Correlation Matrix of Binary Attributes')
+    plt.show()
+
+def countplot_per_feature(df, feature_list):
+    for i, feature_to_exclude in enumerate(feature_list):
+        features_subset = [
+            feature for feature in feature_list if feature != feature_to_exclude]
+
+        """ Countplot for 5 features """
+        fig, axes = plt.subplots(
+            1, len(feature_list)-1, figsize=(20, 3))  # Changed the number of columns to 5
+
+        palette = 'rocket'
+
+        for i, feature in enumerate(features_subset):
+            sns.countplot(data=df, x=feature, hue=feature_to_exclude,
+                          ax=axes[i], palette=palette)
+            axes[i].get_legend().remove()
+            axes[i].tick_params(axis='x', rotation=45)
+
+        plt.tight_layout()
+        plt.suptitle("Binary feature analysis", size=16, y=1.02)
+        plt.legend(title=feature_to_exclude,
+                   bbox_to_anchor=(1.05, 1), loc='upper left')
+        plt.show()
+
+def chi_squared_test(df, feature_tuple):
+    """ Chi-squared test for binary features """
+
+    contingency_table = pd.crosstab(df[feature_tuple[0]], df[feature_tuple[1]])
+
+    chi2, p_value, _, _ = chi2_contingency(contingency_table)
+
+    a = f"Chi-squared statistic: {chi2}."
+    b = f"P-value: {p_value}."
+
+    if p_value < alpha:
+        print(f"{a} {b} Reject the null hypothesis.")
+    else:
+        print(f"{a} {b} Do not reject the null hypothesis.")
+
+
+def biserial_heatmap(df, continues_features, binary_features):
+    """ Biserial correlation for binary and continues features. """
+    correlation_matrix = pd.DataFrame(
+        index=binary_features, columns=continues_features)
+
+
+    for binary_feature in binary_features:
+
+        for continuous_feature in continues_features:
+
+            biserial_corr, _ = stats.pointbiserialr(
+
+                df[binary_feature], df[continuous_feature])
+
+            correlation_matrix.loc[binary_feature,
+
+                                continuous_feature] = biserial_corr
+
+
+    correlation_matrix = correlation_matrix.apply(pd.to_numeric)
+
+
+    sns.heatmap(pd.DataFrame(correlation_matrix),
+                annot=True, cmap="rocket", fmt=".2f")
+
+    plt.title("Biserial Correlation Heatmap")
+
     plt.show()
